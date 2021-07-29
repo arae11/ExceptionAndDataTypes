84acc70732153207091977df01bd4aab02e1e880
Alex Rae <arae@spartaglobal.com>
2021-07-29 14:31:49 +0100

    Greater than 100 test

---
+++ ExceptionsDatatypes/ExceptionDatatypesTest/GradeTests.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using System;
+using ExceptionsApp;
+
+namespace ExceptionDatatypesTest
+{
+    public class GradeTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(-34)]
+        [TestCase(-1)]
+        public void WhenMarkIsLessThanZero_Grade_ThrowsAnArgumentOutOfRangeException(int mark)
+        {
+            Assert.That(()=> Program.Grade(mark), Throws.TypeOf<ArgumentOutOfRangeException>().With.Message.Contains("Allowed range of 0-100")); // this is a generic
+        }
+
+        [TestCase(400)]
+        [TestCase(101)]
+        public void WhenMarkIsGreaterThanOneHundred_Grade_ThrowsAnArgumentOutOfRangeException(int mark)
+        {
+            Assert.That(()=> Program.Grade(mark), Throws.TypeOf<ArgumentOutOfRangeException>().With.Message.Contains("Allowed range of 0-100"));
+        }
+    }
+}
--- ExceptionsDatatypes/ExceptionsApp/Program.cs
+++ ExceptionsDatatypes/ExceptionsApp/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.IO;
 
 namespace ExceptionsApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text;
+            string fileName = "";
+
+            try
+            {
+                Console.WriteLine("Anil's mark is 82: " + Grade(21));
+                Console.WriteLine("Cathy's mark is -23: " + Grade(-23));
+                Console.WriteLine("Jeff's mark is 69: " + Grade(69));
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Invalid data, please try again");
+                Console.WriteLine(e.Message);
+            }
+            
+
+            try
+            {
+                text = File.ReadAllText(fileName);
+                Console.WriteLine(text);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Sorry I can't find: " + fileName);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("You gave me an empty file name - " + e.Message); //e.message returns the name of the exception
+            }
+            finally //finally will always run even if there is a catch
+            {
+                Console.WriteLine("We made it here!");
+            }
+           // var text = File.ReadAllText("HelloWorld.txt"); //throws exception as file doesnt exist (not a programming error)
+        }
+        public static string Grade(int mark)
+        {
+            if (mark < 0 || mark > 100)
+            {
+                throw new ArgumentOutOfRangeException("Mark: " + mark + ", Allowed range of 0-100");
+            }
+            return mark >= 65 ? (mark >= 85 ? "Distinction" : "Pass") : "Fail";
         }
     }
 }
+
+
+
+// exception is an error occuring during runtime
